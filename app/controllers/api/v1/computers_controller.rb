189e9dd701d28696a1bdce9e3526d68d4042c34f
Luiz Miguel <luizmgc1930@gmail.com>
2024-03-05 17:39:48 -0300

    update8

--- app/controllers/api/v1/computers_controller.rb
+++ app/controllers/api/v1/computers_controller.rb
@@ -18,12 +18,31 @@ class Api::V1::ComputersController < ApplicationController
   def create
     @computer = Computer.new(computer_params)
 
-    if @computer.save && valid_component_types?
+    if @computer.save && valid_component_types? && cpu_validate? && ram_validate?
       render json: @computer, status: :created, location: api_v1_computer_url(@computer)
-    elsif cpu_validate?
-      render json: { error: "Falha na validação da CPU" }, status: :unprocessable_entity
     else
-      render json: { error: "Falha na validação dos componentes" }, status: :unprocessable_entity
+      error_messages = []
+
+      unless valid_component_types?
+        error_messages << "Algum componente faltando!"
+        error_messages << "Está faltando processador (CPU)" unless computer_needs_cpu?
+        error_messages << "Está faltando memória RAM" unless computer_needs_ram?
+        error_messages << "Está faltando placa mãe" unless computer_needs_motherboard?
+        error_messages << "É necessário colocar placa de video" unless computer_needs_mb_gpu? || computer_needs_gpu?
+      end
+
+      unless cpu_validate?
+        error_messages << "Falha na validação da CPU:"
+        error_messages << "As marcas do processador e da placa mãe não se correspondem. Escolha a mesma marca para ambas!" unless different_brands?
+      end
+
+      unless ram_validate?
+        error_messages << "Falha na validação da RAM:"
+        error_messages << " - Número de slots de memória excedido." unless enough_memory_slots?
+        error_messages << " - Memória total excede o limite permitido." unless valid_ram_total?
+      end
+      render json: { error: error_messages.join(" ") }, status: :unprocessable_entity
+
     end
   end
 
@@ -46,32 +65,121 @@ class Api::V1::ComputersController < ApplicationController
   private
 
 
-  def cpu_validate?
-    cpu_validate = Component.joins(:component_attribute)
-                            .where('component_attributes.componentType = ?', 'CPU')
-                            .pluck(:brand)
-    mb_validate = Component.joins(:component_attribute)
-                           .where('component_attributes.componentType = ?', 'Motherboard')
-                           .pluck(:cpuSupport)
+  private
 
-puts "cpu_validate: #{cpu_validate}"
-puts "mb_validate: #{mb_validate}"
+  #Validador da memória RAM
+  def ram_validate?
+    component_names = params.dig(:computer, :component_names) || []
 
-    return cpu_validate.sort == mb_validate.sort
+    @mb_memory_slots = Component.joins(:component_attribute)
+                               .where(componentName: component_names)
+                               .where.not(component_attributes: { memorySlot: nil })
+                               .select('DISTINCT component_attributes.memorySlot')
+                               .pluck(:memorySlot)
+                               .first
+
+    @mb_memory_limit = Component.joins(:component_attribute)
+                               .where(componentName: component_names)
+                               .where.not(component_attributes: { memoryLimit: nil })
+                               .select('DISTINCT component_attributes.memoryLimit')
+                               .pluck(:memoryLimit)
+                               .first
+
+    @ram_memory = Component.joins(:component_attribute)
+                          .where(componentName: component_names)
+                          .where.not(component_attributes: { memory: nil })
+                          .select('DISTINCT component_attributes.memory')
+                          .pluck(:memory)
+                          .first
+
+    enough_memory_slots = @mb_memory_slots.to_i >= @computer.qntOfSlot
+
+    enough_memory_slots ? true : false
+
+    @ram_total = @computer.qntOfSlot * @ram_memory.to_i
+
+    return enough_memory_slots && (@mb_memory_limit.to_i >= @ram_total)
   end
+
+  #Validador da CPU
+  def cpu_validate?
+    component_names = params.dig(:computer, :component_names) || []
+
+    @cpu_brands = Component.joins(:component_attribute)
+                         .where(componentName: component_names)
+                         .where.not(component_attributes: { brand: nil })
+                         .select('DISTINCT component_attributes.brand')
+                         .map(&:brand)
+
+    @mb_cpuSupport = Component.joins(:component_attribute)
+                             .where(componentName: component_names)
+                             .where.not(component_attributes: { cpuSupport: nil })
+                             .select('DISTINCT component_attributes.cpuSupport')
+                             .map(&:cpuSupport)
+
+
+    return @mb_cpuSupport.include?("BOTH") ? true : @cpu_brands.sort == @mb_cpuSupport.sort
+
+  end
+
+
+  #Validador dos componentes
   def valid_component_types?
     component_names = params.dig(:computer, :component_names) || []
 
-    required_types = ['RAM', 'CPU', 'Motherboard', 'GPU']
+    @required_types = ['RAM', 'CPU', 'Motherboard']
+    @required_types_with_gpu = ['RAM', 'CPU', 'Motherboard', 'GPU']
 
 
-    unique_types = Component.joins(:component_attribute)
+    @unique_types = Component.joins(:component_attribute)
                             .where(componentName: component_names)
                             .select('DISTINCT component_attributes.componentType')
                             .map(&:componentType)
 
-    return unique_types.sort == required_types.sort
+    @has_video = Component.joins(:component_attribute)
+                          .where(componentName: component_names)
+                          .where.not(component_attributes: {video: nil})
+                          .select('DISTINCT component_attributes.video')
+                          .pluck(:video)
+                          .first
+    #return @unique_types.sort == @@required_types_with_gpu.sort
+    if @has_video
+      return @unique_types.sort == @required_types_with_gpu.sort || @unique_types.sort == @required_types.sort
+    else
+      return @unique_types.sort == @required_types_with_gpu.sort
+    end
   end
+  def computer_needs_motherboard?
+    @required_types.include?('Motherboard') && @unique_types.include?('Motherboard')
+  end
+  def computer_needs_ram?
+    @required_types.include?('RAM') && @unique_types.include?('RAM')
+  end
+  def computer_needs_cpu?
+    @required_types.include?('CPU') && @unique_types.include?('CPU')
+  end
+  def computer_needs_gpu?
+    @required_types_with_gpu.include?('GPU') && @unique_types.include?('GPU')
+  end
+  def computer_needs_mb_gpu?
+    @has_video ?  @unique_types.sort == @required_types.sort : @unique_types.sort == @required_types_with_gpu.sort
+  end
+  #Tratamento de erros
+  #Memória RAM
+  def enough_memory_slots?
+    @mb_memory_slots.to_i >= @computer.qntOfSlot
+  end
+
+  def valid_ram_total?
+    ram_total = @computer.qntOfSlot * @ram_memory.to_i
+    @mb_memory_limit.to_i >= ram_total
+  end
+
+  #Processador - Placa Mãe
+  def different_brands?
+    @cpu_brands.sort == @mb_cpuSupport.sort
+  end
+
   # Use callbacks to share common setup or constraints between actions.
   def set_computer
     @computer = Computer.find(params[:id])
